ba0915169c45a208b92da1b88ba91bea79a99bfe
Stelios Papadopoulos <stylianosplus@gmail.com>
2025-07-06 12:36:19 +0300

    added errors

--- php/index.php
+++ php/index.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 $db = new mysqli(getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASS'), getenv('DB_NAME'));
 echo 'Hello PHP Site! MySQL Status: ' . ($db->connect_error ? $db->connect_error : 'Connected');
+
 ?>
